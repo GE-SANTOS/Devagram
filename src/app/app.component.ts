7636e4cc5d24ca4df7faa41ec45932535a7cfe06
Geovane Santos <geovane_n.s@outlook.com>
2022-07-04 13:49:05 -0300

    Iniciando o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'devagram-angular';
+
+  public form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      imagem: [null],
+      nome: ['', Validators.required],
+      senha: ['', Validators.required],
+    });
+  }
+
+  public aoTrocarImagem() {
+    console.log('imagem alterada');
+  }
+
+  public obterReferencia(nomeCampo: string): AbstractControl {
+    return this.form.controls[nomeCampo];
+  }
+
+  public submit(): void {
+    console.log(this.form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { CompartilhadoModule } from './compartilhado/compartilhado.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CompartilhadoModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
